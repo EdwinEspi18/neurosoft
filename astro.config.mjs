9032daa67144f5fb45e21f03b8deb69d99208d7a
Edwin A. Espinal Taveras <espinaledwin1919@gmail.com>
2024-09-27 10:11:42 -0400

    ...

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://neurosoft.vercel.app/",
   integrations: [preact()],
   vite: {
     envPrefix: "EMAIL_",
