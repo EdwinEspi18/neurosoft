2ad32d59f156ebd9fb274c0af090e9510689460f
Edwin A. Espinal Taveras <espinaledwin1919@gmail.com>
2024-09-26 23:51:36 -0400

    adapeter vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,16 @@ import robotsTxt from "astro-robots-txt";
 
 import preact from "@astrojs/preact";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [robotsTxt(), preact()],
+
   vite: {
     envPrefix: "EMAIL_",
   },
+
   output: "server",
+  adapter: vercel(),
 });
